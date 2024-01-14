fbdc7e89e7daa9ef3196afc508204b7010a34ca5
Nikola Nikolov <122821901+NikolaNikolov93@users.noreply.github.com>
2024-01-14 11:38:17 +0200

    Initialize portfolio project

---
+++ nikola-portfolio/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import Sidenav from "./components/Sidenav";
+
+function App() {
+    return (
+        <>
+            <Sidenav />
+        </>
+    );
+}
+
+export default App;
